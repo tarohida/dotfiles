1f15ca39df80385926e78bf4ba750ae7ac8e0358
taro-hida <taro-hida>
2018-12-17 21:42:33 +0900

    developping

--- install.sh
+++ install.sh
@@ -1,9 +1,17 @@
+git config --global user.name taro-hida
+git config --global user.email hidataro11@gmail.com
 mkdir -p ~/.vim/bundle
 git clone https://github.com/Shougo/neobundle.vim  ~/.vim/bundle/neobundle.vim
+if [ -e ~/.vimrc ] ; then
+  mv ~/.vimrc{,.bak}
+fi
 ln -Fis ~/dotfiles/.vimrc ~/.vimrc
-ln -Fis ~/dotfiles/vimfiles ~/.vim
+if [ -e ~/.vim ] ; then
+  mv ~/.vim{,.bak}
+fi
+ln -Fis ~/dotfiles/vimfiles~/.vim
+if [ -e ~/.bashrc ] ; then
+  mv ~/.bashrc{,.bak}
+fi
 ln -Fis ~/dotfiles/.bashrc ~/.bashrc
 cd ~/dotfiles
-git config --global user.email hidataro11@gmail.com
-git config --global user.email taro-hida
-
