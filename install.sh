b0a62f402e045635bc3e7a0bfd8a80d7171760e7
taro-hida <hidataro11@gmail.com>
2019-01-23 06:12:11 +0900

    change dir that create backup

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@ git config --global user.name taro-hida
 git config --global user.email hidataro11@gmail.com
 
 if [ -e ~/.vim ] ; then
-  mv -iv ~/.vim{,.org}
+  mv -iv ~/dotfile/.vim{,.org}
 fi
 mkdir -vp ~/.vim/bundle
 git clone https://github.com/Shougo/neobundle.vim  ~/.vim/bundle/neobundle.vim
@@ -10,11 +10,11 @@ mv -iv ~/.vim ~/dotfiles/vimfiles
 ln -ivs ~/dotfiles/vimfiles ~/.vim
 
 if [ -e ~/.vimrc ] ; then
-  mv -iv ~/.vimrc{,.org}
+  mv -iv ~/dotfile/.vimrc{,.org}
 fi
 ln -ivs ~/dotfiles/vimrc ~/.vimrc
 if [ -e ~/.bashrc ] ; then
-  mv -iv ~/.bashrc{,.org}
+  mv -iv ~/dotfile/.bashrc{,.org}
 fi
 ln -ivs ~/dotfiles/bashrc ~/.bashrc
 cd ~/dotfiles
