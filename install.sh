87b91211761e4def6138bc7a38975309308c4d69
taro-hida <hidataro11@gmail.com>
2019-03-15 18:36:47 +0000

    fix

--- install.sh
+++ install.sh
@@ -2,19 +2,20 @@ git config --global user.name taro-hida
 git config --global user.email hidataro11@gmail.com
 
 if [ -e ~/.vim ] ; then
-  mv -iv ~/dotfile/.vim{,.org}
+  mv -ivr ~/.vim ~/dotfiles/vim.org
 fi
-mkdir -vp ~/.vim/bundle
-git clone https://github.com/Shougo/neobundle.vim  ~/.vim/bundle/neobundle.vim
-mv -iv ~/.vim ~/dotfiles/vimfiles
+mkdir -vp ~/dotfiles/vimfiles/bundle
+git clone https://github.com/Shougo/neobundle.vim  ~/dotfiles/vimfiles/bundle/neobundle.vim
 ln -ivs ~/dotfiles/vimfiles ~/.vim
 
 if [ -e ~/.vimrc ] ; then
-  mv -iv ~/dotfile/.vimrc{,.org}
+  mv -iv ~/.vimrc ~/dotfiles/vimrc.org
 fi
 ln -ivs ~/dotfiles/vimrc ~/.vimrc
+
 if [ -e ~/.bashrc ] ; then
-  mv -iv ~/dotfile/.bashrc{,.org}
+  mv -iv ~/.bashrc ~/dotfiles/bashrc.org
 fi
 ln -ivs ~/dotfiles/bashrc ~/.bashrc
+
 cd ~/dotfiles
