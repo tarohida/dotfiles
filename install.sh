1aed37fca7f983bd647ebe1ce6aac293f02d97b6
taro-hida <hidataro11@gmail.com>
2019-09-18 08:30:05 +0000

    vimrcについて、amix/vimrcを使用するように変更

--- install.sh
+++ install.sh
@@ -1,24 +1,20 @@
 git config --global user.name taro-hida
 git config --global user.email hidataro11@gmail.com
 
-if [ -e ~/.vim ] ; then
-  mv -ivr ~/.vim ~/dotfiles/vim.org
-fi
-mkdir -vp ~/dotfiles/vimfiles/bundle/
-git clone https://github.com/Shougo/neobundle.vim  ~/dotfiles/vimfiles/bundle/neobundle.vim
-ln -ivs ~/dotfiles/vimfiles ~/.vim
-
-mkdir -vp ~/dotfiles/vimfiles/autoload/
-curl -O https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-
+# Evacuate old vimrc
 if [ -e ~/.vimrc ] ; then
   mv -iv ~/.vimrc ~/dotfiles/vimrc.org
 fi
-ln -ivs ~/dotfiles/vimrc ~/.vimrc
 
+# setup vimrc
+git clone --depth=1 https://github.com/amix/vimrc.git ~/.vim_runtime
+sh ~/.vim_runtime/install_awesome_vimrc.sh
+
+# Evacuate and install bashrc
 if [ -e ~/.bashrc ] ; then
   mv -iv ~/.bashrc ~/dotfiles/bashrc.org
 fi
 ln -ivs ~/dotfiles/bashrc ~/.bashrc
 
 cd ~/dotfiles
+source bashrc
