486252a4529e89d33bfd01d8501ee1a949255a76
tarohida <sk8trou@gmail.com>
2024-04-14 00:30:06 +0900

    既存の .vimrc, .bashrc はバックアップするのみで、活用しないことにする

--- setup.sh
+++ setup.sh
@@ -6,22 +6,23 @@ then
     exit 1
 fi
 
-# Evacuate old vimrc
+backup_dir="backup_$(date +"%Y%m%d%H%M%S")"
+mkdir "$backup_dir"
+
+# .vimrc をバックアップディレクトリに移動
 if [ -e ~/.vimrc ] ; then
-  mv -iv ~/.vimrc ~/dotfiles/vimrc.org
+  mv -iv ~/.vimrc "$backup_dir"/
+fi
+
+# .bashrc をバックアップディレクトリに移動
+if [ -e ~/.bashrc ] ; then
+  mv -iv ~/.bashrc "$backup_dir"/
 fi
 
 # setup vimrc
 git clone --depth=1 https://github.com/amix/vimrc.git ~/.vim_runtime
 sh ~/.vim_runtime/install_awesome_vimrc.sh
 
-# Evacuate and install bashrc
-if [ -e ~/.bashrc ] ; then
-  echo ". ~/dotfiles/bashrc.org" >>  ~/.bashrc
-else
-  ln -ivs ~/dotfiles/bashrc ~/.bashrc
-fi
+ln -vs ~/dotfiles/bashrc ~/.bashrc
 
 . ~/.bashrc
-
-cd ~/dotfiles
