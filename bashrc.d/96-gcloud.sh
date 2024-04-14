31abe1876751fac91a9825f266803fc3ea99b9cd
tarohida <sk8trou@gmail.com>
2024-04-14 10:26:01 +0900

    rocky9 のインスタンスを作成する処理を追加

--- bashrc.d/96-gcloud.sh
+++ bashrc.d/96-gcloud.sh
@@ -17,4 +17,23 @@ gcloud-wrapper () {
     fi
 }
 
+gcloud-create-instance() {
+    name=$1
+    image_project=$2
+    image_family=$3
+    gcloud compute instances create $name \
+    --boot-disk-auto-delete \
+    --boot-disk-size=20GB \
+    --boot-disk-type=pd-standard \
+    --zone=asia-northeast2-a \
+    --image-project="$image_project" \
+    --image-family="$image_family" \
+    --machine-type=n1-standard-1
+}
+
+gcloud-create-rocky9() {
+    suffix=$1
+    gcloud-create-instance rocky9-$1-"$(date '+%Y%m%d%H%M%S')" rocky-linux-cloud rocky-linux-9
+}
+
 alias gcloud="gcloud-wrapper"
